a3ea1597a1ad9bf9fb4c14cdc7cd55ca932a698a
NgoThanhDe1904 <19130035@st.hcmuaf.edu.vn>
2023-11-07 19:35:20 +0700

    update tạm

---
+++ assets/js/updateUrl.js
@@ -0,0 +1,28 @@
+const updateBtn = document.querySelector('.js-update-url')
+const update = document.querySelector('.update')
+const updateContainer = document.querySelector('.update-container')
+const updateClose= document.querySelector('.js-update-close')
+
+// Hàm hiển thị update ( Thêm class vào update)
+function showUpdate(){
+  update.classList.add('open')
+}
+
+// hàm ẩn modal mua vé ( Gỡ bỏ class open của modal)
+function hideUpdate(){
+  update.classList.remove('open')
+}
+
+// Lặp qua từng thẻ button và nghe hành vi và click
+for(const updateBtn of updateBtn){
+  updateBtn.addEventListener('click', showUpdate())
+}
+
+// Nghe hành vi và click button close
+updateClose.addEventListener('click', hideUpdate())
+
+update.addEventListener('click', hideUpdate())
+
+updateContainer.addEventListener('click', function (event){
+  event.stopPropagation()
+})
